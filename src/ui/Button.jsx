ec91de0ef180a03c5559cf1869fd2dd8b45a3fbe
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-15 04:11:29 +0300

    Style Button.jsx
    Adding hover style using styled-components
    
    (Please note that '&' refrest to the element itself)

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -57,6 +57,9 @@ const Button = styled.button`
   color: var(--color-brand-50);
   box-shadow: var(--shadow-sm);
   cursor: pointer;
+  &:hover {
+    background-color: var(--color-brand-700);
+  }
 `;
 
 export default Button;
