1c68332381b46515fd1fdb6bb4824f26c3056d9b
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-15 10:06:32 +0300

    Sidebar links
    settings navlinks onsidebar component using react navlink component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
@@ -8,7 +9,12 @@ const StyledSidebar = styled.aside`
 `;
 
 function SideBar() {
-  return <StyledSidebar>SideBar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default SideBar;
