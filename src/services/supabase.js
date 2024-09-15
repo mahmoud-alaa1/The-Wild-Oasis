92d5e56e0637298a6c51f6a83b985e5ade74eae8
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-15 14:05:28 +0300

    Setting supabase
    I have created supabase.js file to make supabase clinet so that I can call my database anywhere

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://opfszxzgozmskidrfxac.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wZnN6eHpnb3ptc2tpZHJmeGFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzOTExMDMsImV4cCI6MjA0MTk2NzEwM30.z5MpzH3HckwD5HdS_xCnn3j-j5vXxF3Rsy4plH2mwoQ";
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+  
+export default supabase;
