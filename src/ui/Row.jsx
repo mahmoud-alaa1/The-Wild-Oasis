652fc9b372177e6f6719ac0e827b372c19502cb1
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-15 09:25:35 +0300

    Reuseable Component, Row

---
+++ src/ui/Row.jsx
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  ${(props) =>
+    props.type == "horizontal" &&
+    css`
+      justify-content: space-between;
+      align-items: center;
+    `}
+  ${(props) =>
+    props.type == "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
+  ${(props) => console.log(props)}
+`;
+
+Row.defaultProps = {
+  type: "vertical",
+};
+
+export default Row;
