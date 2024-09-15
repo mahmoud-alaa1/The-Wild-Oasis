bef4045ecb45e87fb2138119f32845c2ef02670c
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-15 04:09:50 +0300

    Reusable Components
    I made Input.jsx and Button.jsx using styled-components

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,16 @@ const variations = {
     }
   `,
 };
+const Button = styled.button`
+  font-family: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+`;
+
+export default Button;
