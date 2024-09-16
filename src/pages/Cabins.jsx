06d3f3789ca2629aeb96ef5376e08a9410d50af4
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-16 07:34:35 +0300

    New Library, React hook form
    that library is responsible for creating cabin form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,11 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import { useState } from "react";
+import Button from "../ui/Button";
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -10,6 +14,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((showForm) => !showForm)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm></CreateCabinForm>}
       </Row>
     </>
   );
