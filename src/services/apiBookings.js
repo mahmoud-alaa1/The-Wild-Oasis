e6936ef61961bb027eb3d62d546100295573b363
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-20 14:25:13 +0300

    Change Bookings Query
    I have changed Bookings Query to get less data (skip unnecessary data) for optimizations
    the data fetched now is:
    
    Bookings: id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice
    
    cabins: name
    
    guests: fullName, Email

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -2,7 +2,11 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 export async function getBookings() {
-  const { data, error } = await supabase.from("bookings").select("*");
+  const { data, error } = await supabase
+    .from("bookings")
+    .select(
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)"
+    );
 
   console.log(data);
   if (error) {
