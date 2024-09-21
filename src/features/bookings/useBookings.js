3cc54edee483f93236cc0c3c3a1a229411ac9886
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-21 03:34:28 +0300

    Feature, Prefetch Query
    I have used preftch query for pagination for better user-experiece (no loading spinner on browsing table pages)
    
    I prefetch next page and previous page
    
    We can use infnite scroll insted of pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 export function useBookings() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const queryClient = useQueryClient();
+
+  const [searchParams] = useSearchParams();
 
   //(1)FILTER
   const filterValue = searchParams.get("status");
@@ -17,9 +20,10 @@ export function useBookings() {
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
-  //(3) Pagination
+  //(3) PAGINATION
   const page = Number(searchParams.get("page")) || 1;
 
+  //(4) QUERY
   const {
     data: { data: bookings, count } = {},
     isPending,
@@ -28,6 +32,19 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  //PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return {
     bookings,
