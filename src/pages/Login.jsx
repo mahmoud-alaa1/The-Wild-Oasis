6a2468d1393b3c78d7fd8a6ffa746cd0f9ee0761
Mahmoud Alaa <asticalaa1@gmail.com>
2024-09-23 22:04:12 +0300

    Style
    i made login page responsive by making media queries at predefined breakpoints at constants.js

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,22 +2,44 @@ import styled from "styled-components";
 import LoginForm from "../features/authentication/LoginForm";
 import Logo from "../ui/Logo";
 import Heading from "../ui/Heading";
+import { device } from "../utils/constants";
 const LoginLayout = styled.main`
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: 48rem;
+  height: 100vh;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  background-color: var(--color-grey-50);
+  padding: 2rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   align-content: center;
   justify-content: center;
   gap: 3.2rem;
-  background-color: var(--color-grey-50);
+  width: 40%;
+  @media ${device.laptop} {
+    width: 50%;
+  }
+  @media ${device.tablet} {
+    width: 70%;
+  }
+  @media ${device.mobileL} {
+    width: 90%;
+    align-items: center;
+  }
+  margin: auto;
 `;
 
 function Login() {
   return (
     <LoginLayout>
-      <Logo />
-      <Heading as="h3">Log in to your account</Heading>
-      <LoginForm />
+      <Container>
+        <Logo />
+        <Heading as="h3">Log in to your account</Heading>
+        <LoginForm />
+      </Container>
     </LoginLayout>
   );
 }
